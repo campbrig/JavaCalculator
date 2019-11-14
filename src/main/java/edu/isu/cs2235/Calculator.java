681f4b03439bcd3425ea9a4ffacfdbae0d40d716
Brigham Campbell <me@brighamcampbell.com>
2019-11-14 10:23:32 -0700

    Implemented Calculator functions

--- src/main/java/edu/isu/cs2235/Calculator.java
+++ src/main/java/edu/isu/cs2235/Calculator.java
@@ -3,5 +3,29 @@ package edu.isu.cs2235;
 import java.util.Stack;
 
 public class Calculator {
-    private Stack<int>
+    private Stack<Float> history = new Stack<Float>();
+
+    public float add(float value) {
+        float result = history.peek() + value;
+        history.push(result);
+        return result;
+    }
+
+    public float subtract(float value) {
+        float result = history.peek() - value;
+        history.push(result);
+        return result;
+    }
+
+    public float multiply(float value) {
+        float result = history.peek() * value;
+        history.push(result);
+        return result;
+    }
+
+    public float divide(float value) {
+        float result = history.peek() / value;
+        history.push(result);
+        return result;
+    }
 }
