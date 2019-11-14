89a7a516d64b849261918a730c40758163b5596c
Dylan Brown <Dman2210@gmail.com>
2019-11-14 10:24:51 -0700

    created tests file

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,50 @@
+public class CalculatorTest {
+    package edu.isu.cs2235.structures.impl;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+    public class CalculatorTest {
+        private Calculator fixture;
+
+        @BeforeClass
+        public static void setUpClass() {
+        }
+
+        @AfterClass
+        public static void tearDownClass() {
+        }
+
+        @Before
+        public void setUp() {
+            fixture = new LinkedQueue<>();
+        }
+
+        @After
+        public void tearDown() {
+        }
+
+        @Test
+        public void testAdd(){}
+
+        @Test
+        public void testSubtract(){}
+
+        @Test
+        public void testDivide(){}
+
+        @Test
+        public void testMultiply(){}
+
+        @Test
+        public void testHistory(){}
+
+    }
+}
